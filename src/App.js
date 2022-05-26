3675acac18ff15de57911e6cb87166575e4c1418
Sergey Kuznetsov <mr.enline@gmail.com>
2022-05-26 18:25:25 +0300

    memo and PureComponent

--- src/App.js
+++ src/App.js
@@ -1,67 +1,126 @@
-import { useState, useEffect} from 'react';
-import {Container} from 'react-bootstrap';
+import { useState, memo, PureComponent } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
-function useInputWithValidate(inititalValue) {
-    const [value, setValue] = useState(inititalValue);
-
-    const onChange = e => {
-        setValue(e.target.value);
-    }
-
-    const validateInput = () => {
-        return value.search(/\d/) >= 0;
-    }
-
-    return {value, validateInput, onChange} //аналогично {value: value, validateInput: validateInput, onChange: onChange}
+function propsCompare(prevprops, nextprops) {
+    return (
+        prevprops.mail.name === nextprops.mail.name &&
+        prevprops.text === nextprops.text
+    );
 }
 
-const Form = () => {
-
-    // const [text, setText] = useState('');
-    // const [textArea, setTextArea] = useState('');
-    
-    // const validateInput = (text) => {
-    //     return text.search(/\d/) >= 0;
-    // }
-
-    const input = useInputWithValidate('');
-    const textArea = useInputWithValidate('');
-
-    const color = input.validateInput() ? 'text-danger' : null;
-
+const Form = memo((props) => {
+    console.log('render');
     return (
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
-                    <input value={`${input.value} / ${textArea.value}`} type="text" className="form-control" readOnly/>
-                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <input 
-                        onChange={input.onChange} 
-                        value={input.value}
-                        type="email" 
-                        className={`form-control ${color}`} 
-                        id="exampleFormControlInput1" 
-                        placeholder="name@example.com"/>
-                    </div>
-                    <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea 
-                        onChange={textArea.onChange}
-                        value={textArea.value}
-                        className="form-control" 
-                        id="exampleFormControlTextarea1" 
-                        rows="3">
-                    </textarea>
+                    <label
+                        htmlFor="exampleFormControlInput1"
+                        className="form-label mt-3"
+                    >
+                        Email address
+                    </label>
+                    <input
+                        value={props.mail.name}
+                        type="email"
+                        className="form-control"
+                        id="exampleFormControlInput1"
+                        placeholder="name@example.com"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label
+                        htmlFor="exampleFormControlTextarea1"
+                        className="form-label"
+                    >
+                        Example textarea
+                    </label>
+                    <textarea
+                        value={props.text}
+                        className="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="3"
+                    ></textarea>
                 </div>
             </form>
         </Container>
-    )
-}
+    );
+}, propsCompare);
+
+//PureComponent производит поверхностное копирование как и memo
+//поэтому лучше использовать componentDidUpdate и в нем уже проверять пропсы
+//и если они изменились, то выполнять перерендер
+// class Form extends PureComponent {
+
+//componentDidUpdate(prevProps) {
+// if (prevProps.mail.name === props.mail.name) {
+//     return false
+// }return true
+//}
+
+//     render() {
+//             console.log('render');
+//             return (
+//                 <Container>
+//                     <form className="w-50 border mt-5 p-3 m-auto">
+//                         <div className="mb-3">
+//                             <label
+//                                 htmlFor="exampleFormControlInput1"
+//                                 className="form-label mt-3"
+//                             >
+//                                 Email address
+//                             </label>
+//                             <input
+//                                 value={props.mail.name}
+//                                 type="email"
+//                                 className="form-control"
+//                                 id="exampleFormControlInput1"
+//                                 placeholder="name@example.com"
+//                             />
+//                         </div>
+//                         <div className="mb-3">
+//                             <label
+//                                 htmlFor="exampleFormControlTextarea1"
+//                                 className="form-label"
+//                             >
+//                                 Example textarea
+//                             </label>
+//                             <textarea
+//                                 value={props.text}
+//                                 className="form-control"
+//                                 id="exampleFormControlTextarea1"
+//                                 rows="3"
+//                             ></textarea>
+//                         </div>
+//                     </form>
+//                 </Container>
+//             );
+//     }
+// }
 
 function App() {
+    const [data, setData] = useState({
+        mail: {
+            name: 'name@example.com',
+        },
+        text: 'some text',
+    });
+
     return (
-        <Form/>
+        <>
+            <Form mail={data.mail.name} text={data.text} />
+            <button
+                onClick={() =>
+                    setData({
+                        mail: { name: 'second@example.com' },
+                        text: 'another text',
+                    })
+                }
+            >
+                Click me
+            </button>
+        </>
     );
 }
 
