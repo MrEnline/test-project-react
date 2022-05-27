c9bf245376f25f75fc4332ddcd64579cd1e46f85
Sergey Kuznetsov <mr.enline@gmail.com>
2022-05-27 18:08:28 +0300

    context functional components

--- src/App.js
+++ src/App.js
@@ -12,10 +12,14 @@ function App() {
         forceChangeMail: forceChangeMail,
     });
 
+    //с помощью этой функции можем изменять состояние пропса, которое передается через контекст
+    //важно добавить ссылку на эту функцию в само состояние, чтобы потом можно было вызвать
+    //ее в дочерних компонентах
     function forceChangeMail() {
         setData({ ...data, mail: 'test@mail.ru' });
     }
 
+    //если не указать в Provider value, то будет передано значение по умолчанию из context.js
     return (
         <Provider value={data}>
             <Form text={data.text} />
